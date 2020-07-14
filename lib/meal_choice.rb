9208693e0b8ffc62d4b4d329551922e3d4a63171
JoAnna Park <joannabpark1@gmail.com>
2020-07-14 17:53:12 +0000

    Automatically backed up by Learn

--- lib/meal_choice.rb
+++ lib/meal_choice.rb
@@ -8,4 +8,4 @@ def meal_choice(veg1, veg2, protein = "tofu")
   "A plate of #{protein} with #{veg1} and #{veg2}."
 end
 
-meal_choice ("broccoli", "macaroni")
+meal_choice ("broccoli", "macaroni", protein)
